17de3d4def83368e7bd32eeeacaa04ad438ab05b
josemiguel28 <miguel@miguel.>
2025-02-18 18:08:52 -0600

    added migration

--- database/migrations/2025_01_17_035046_create_vacante_table.php
+++ database/migrations/2025_01_17_035046_create_vacante_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->dropForeign(['categoria_id']);
         });
     }
 
