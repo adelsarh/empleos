ee2ea3db4da2aa9e1780e6bc61f19fe3d1734e64
miguel ㅤ <miguel@MacBook-Air-de-miguel.local>
2025-04-11 16:20:26 -0600

    testing commit

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
+// Zona de administración y reclutador
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified', 'checkUserRole'])
     ->name('vacantes.index');
 Route::get('/vacante/crear', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
@@ -19,13 +20,13 @@ Route::get('/vacante/{vacante}/edit', [VacanteController::class, 'edit'])->middl
 Route::get('/vacante/{vacante}', [VacanteController::class, 'show'])->name('vacantes.show');
 Route::get('/notificaciones', NotificacionController::class)->middleware(['auth', 'verified', 'checkUserRole'])->name('notificaciones');
 Route::get('/candidato/{vacante}', [CandidatoController::class,'index'])->middleware(['auth', 'verified'])->name('candidatos.index');
+
+// Zona de administración
 Route::resource('user', GestionCuentasController::class);
 Route::get('/transaccion', [TransaccionController::class, 'index'])->middleware(['auth', 'verified'])->name('transaccion.index');
 Route::get('/plan', [PlanController::class, 'index'])->middleware(['auth', 'verified'])->name('plan.index');
 Route::get('/plan/{plan}', [PlanController::class, 'show'])->middleware(['auth', 'verified'])->name('plan.show');
 Route::post('/planes/{plan}/comprobante', [PlanController::class, 'storeComprobante'])->middleware(['auth', 'verified'])->name('plan.store.comprobante');
-
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
